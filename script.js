4fac140c2e5c310e2cb268bd20f7173a6b0cc903
giulianaa23 <giulianacristea06@gmail.com>
2024-04-22 17:17:11 +0300

    joc

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Declarăm variabile folosind let 
+let maxDistance = 200;
+let stepX = 5;
+let stepY = 5;
+
 function setup() {
     createCanvas(400, 400); // Setare dimensiune canvas
     noStroke(); // Fără contur pentru pătrate
@@ -6,12 +11,12 @@ function setup() {
 
 function draw() {
     clear(); // Curățare canvas
-    for (var gridY = 0; gridY < height; gridY += stepY) {
-        for (var gridX = 0; gridX < width; gridX += stepX) {
+    for (let gridY = 0; gridY < height; gridY += stepY) {
+        for (let gridX = 0; gridX < width; gridX += stepX) {
             // Calculați distanța dintre puncte
-            var distanceBetweenPoints = dist(mouseX, mouseY, gridX, gridY);
+            let distanceBetweenPoints = dist(mouseX, mouseY, gridX, gridY);
             // Ajustați dimensiunea pătratului în funcție de distanță
-            var diameter = distanceBetweenPoints / maxDistance * 60;
+            let diameter = distanceBetweenPoints / maxDistance * 60;
             // Desenați pătratul
             rect(gridX, gridY, diameter, diameter);
         }
